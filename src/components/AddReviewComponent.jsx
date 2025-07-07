2d121603a66a6ad722f672791b0c1476838c4260
Sabarish-2 <sabarishh03@gmail.com>
2025-07-07 09:36:42 +0530

    Implement form validation for review submission

--- src/components/AddReviewComponent.jsx
+++ src/components/AddReviewComponent.jsx
@@ -5,10 +5,43 @@ import { addReview } from "../services/ReviewService";
 const AddReviewComponent = ({setUserReview, bookId}) => {
   const [comment, setComment] = useState("");
   const [rating, setRating] = useState(0);
-  const stars = [1, 2, 3, 4, 5];
+  const [errors, setErrors] = useState({ rating: "", comment: "" });
+
+  const handleEnter = () => {}
+
+  const validateForm = () => {
+    let valid = true;
+    const errorsTemp = {... errors};
+    if (rating == 0) {
+      errorsTemp.rating = "Rating Is Required!";
+      valid = false;
+    } else {
+      errorsTemp.rating = "";
+    }
+    setComment(comment.trim());
+    if (comment === "") {
+      errorsTemp.comment = "Comment Is Required.";
+      valid = false;
+    } else if (comment.length < 3) {
+      errorsTemp.comment = "Comment Must Be Atleast 3 Characters!";
+      valid = false;
+    } else if ((comment[0] < 'a' || comment[0] > 'z') && (comment[0] < 'A' || comment[0] > 'Z')) {
+      errorsTemp.comment = "Comment Must Start With A Letter!";
+      valid = false;
+    } else if (comment.length >= 2000) {
+      errorsTemp.comment = "Comment Must Be Less Than 2000 Characters!";
+      valid = false;
+    } else {
+      errorsTemp.comment = "";
+    }
+    return valid;
+  }
 
   const submitReview = (e) => {
     e.preventDefault();
+    if (!validateForm()) {
+      return;
+    }
     const userId = sessionStorage.getItem("userId");
     const review = {reviewId: 0, rating: rating, comment: comment, userId: userId, bookId: bookId};
     
@@ -43,19 +76,19 @@ const AddReviewComponent = ({setUserReview, bookId}) => {
           <form>
             <div className="fa-2x mb-3">
               <StarsComponent handleRatingClick={handleRatingClick} rating={rating} />
+              {errors.rating && <div className="invalid-feedback">{errors.rating}</div>}
             </div>
             <div className="mb-3">
-              <label htmlFor="reviewComment" className="form-label">
-                <strong>Comment:</strong>
-              </label>
+              <label htmlFor="reviewComment" className="form-label"><strong>Comment:</strong></label>
               <textarea
-                className="form-control"
+                className={`form-control` + (errors.comment) && ` is-invalid`}
                 id="reviewComment"
                 placeholder="Your Comment Here!"
                 onChange={(e) => {
                   setComment(e.target.value);
                 }}
               ></textarea>
+              {errors.comment && <div className="invalid-feedback">{errors.comment}</div>}
             </div>
             <button type="submit" className="btn btn-success" onClick={(e) => submitReview(e)}>
               Add Review
